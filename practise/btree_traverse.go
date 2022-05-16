98857d3d75d72e36dac6274fc78c9eacbd7bac53
hp <allthatjazz@outlook.com>
2022-05-16 12:09:37 +0800

    Preorder traverse

--- practise/btree_traverse.go
+++ practise/btree_traverse.go
@@ -1,10 +1,28 @@
 package practise
 
 import (
-  "fmt"
-  "dsa_go/easy"
+	"dsa_go/easy"
+	"fmt"
 )
 
-func Preorder(root *easy.TreeNode){
-  fmt.Println("hello preorder")
+func Preorder(root *easy.TreeNode) {
+	stack := []*easy.TreeNode{}
+	current := root
+
+	for current != nil || len(stack) > 0 {
+		for current != nil {
+			fmt.Printf("%v,", current.Val)
+
+			stack = append(stack, current)
+			current = current.Left
+		}
+
+		var last = len(stack)
+		if last > 0 {
+			current = stack[last-1]
+			stack = stack[:last-1]
+			current = current.Right
+		}
+	}
+  fmt.Println()
 }
--- practise/btree_traverse_test.go
+++ practise/btree_traverse_test.go
@@ -1,13 +1,34 @@
 package practise
 
 import (
-	"dsa_go/easy"
+	. "dsa_go/easy"
 	"testing"
 )
 
 func TestPreorder(t *testing.T) {
-	var root = easy.TreeNode{
-		Val: 12,
+	var root = TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 6,
+					Left: &TreeNode{
+						Val: 7,
+					},
+					Right: &TreeNode{
+						Val: 8,
+					},
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 5,
+			},
+		},
 	}
 
 	Preorder(&root)
